7520ce41869cee639c8b79362f7d1c7037ae5f0a
Keith Nicholson <kjnicholson16@gmail.com>
2020-11-18 12:32:23 -0500

    commit tree 3538975d92ddc0d2558d1d7e484bce11c5aad1f4

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
+def sort_array_asc(array)
+    array.sort do |a, b|
+        a <=> b
+    end
+end
 
+def sort_array_desc(array)
+    array.sort.reverse do |a, b|
+        a <=> b
+    end
+end
+
+def sort_array_char_count(array)
+    array.sort_by { |string| string.length}
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    kesha = []
+    array.each do |word|
+      word_array = word.split ""
+      word_array[2] = "$"
+      kesha << word_array.join
+    end
+    kesha
+  end
+  
+def find_a(array)
+array.select {|word| word.start_with?('a')}
+end
+
+def sum_array(array)
+    array.inject {|sum, num| sum + num}
+end
+
+def add_s(array)
+    array.each_with_index.collect do |string,index|
+    if index == 1
+        string
+    else
+        string << "s"
+    end
+end
+end
